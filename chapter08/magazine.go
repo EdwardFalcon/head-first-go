4a8795c6654aa5eb3d8683d8bdecf21a81398198
EdwardFalcon <devedwardfalcon@gmail.com>
2021-03-29 11:25:45 +0300

    finish chapter 8

---
+++ chapter08/magazine.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"magazine"
+)
+
+func main() {
+	var employee magazine.Employee
+	employee.Name = "Joy Carr"
+	employee.Salary = 60000
+	fmt.Println(employee.Name)
+	fmt.Println(employee.Salary)
+}
